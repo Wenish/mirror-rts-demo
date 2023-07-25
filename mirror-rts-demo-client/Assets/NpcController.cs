46cb0a62a8a84e37dd721439278623f4b48c69f9
Wenish <Wenish@users.noreply.github.com>
2023-07-25 17:11:27 +0200

    update player

--- mirror-rts-demo-client/Assets/NpcController.cs
+++ mirror-rts-demo-client/Assets/NpcController.cs
@@ -6,14 +6,21 @@ using UnityEngine;
 public class NpcController : NetworkBehaviour
 {
     public Vector3 startPosition;
-    public float deathRadius = 100;
+    public float deathRadius = 20;
     public float speed = 1;
+
+    Vector3 direction;
     // Start is called before the first frame update
     
     void Start()
     {
         if (!isServer) return;
+        transform.position = new Vector3(Random.Range(-30, 30), 0, Random.Range(-30, 30));
+        transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
         startPosition = transform.position;
+        speed = Random.Range(5, 10);
+        direction = transform.right;
+
     }
 
     // Update is called once per frame
@@ -27,13 +34,17 @@ public class NpcController : NetworkBehaviour
     void Move()
     {
         Debug.Log("Npc Move");
-        transform.position += transform.right * (Time.deltaTime * speed);
-        float distance = Vector3.Distance(transform.position, startPosition);
+        var newPostion = transform.position + (direction * (Time.deltaTime * speed));
+        float distance = Vector3.Distance(newPostion, startPosition);
         if (distance > deathRadius)
         {
-            if (isServer) {
-                DestroySelf();
+            if (direction == transform.right) {
+                direction = -transform.right;
+            } else {
+                direction = transform.right;
             }
+        } else {
+            transform.position = newPostion;
         }
     }
 
